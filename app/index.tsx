e79b0841da67c2ef202f8526dad10c82c4c2fc62
Mohd Areeb Safvi <47021490+Areeb0206@users.noreply.github.com>
2025-04-02 13:46:52 +0530

    created select language screen

--- app/index.tsx
+++ app/index.tsx
@@ -20,7 +20,7 @@ const App = () => {
   // if user is not logged in, redirect to login page
   if (!user?.uuid) {
     // return <Redirect href={routes.auth.auth as Href} />;
-    return <Redirect href={routes.tabs.home as Href} />;
+    return <Redirect href={routes.auth.auth as Href} />;
   }
   // if user is logged in, redirect to portfolio page
   if (user?.uuid) {
