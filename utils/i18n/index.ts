b9f8bc1502f4539c5964637a8fdc8e563a401765
Mohd Areeb Safvi <47021490+Areeb0206@users.noreply.github.com>
2025-04-02 07:53:26 +0530

    added language setup

---
+++ utils/i18n/index.ts
@@ -0,0 +1,19 @@
+import { I18n } from 'i18n-js';
+import { getLocales } from 'expo-localization';
+import en from './locales/en.json';
+import sa from './locales/sa.json';
+
+export const deviceLanguage = getLocales()?.[0]?.languageCode ?? 'en';
+
+export const i18n = new I18n({
+  en,
+  sa,
+});
+
+i18n.defaultLocale = deviceLanguage;
+
+i18n.locale = deviceLanguage;
+
+export function changeLanguage(lang: string) {
+  i18n.locale = lang;
+}
